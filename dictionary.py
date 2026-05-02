c64bf1bd7f2bc67cbb95a7a415926ab2b7289ace
rohit-356 <rohitsharma88882@gmail.com>
2026-05-02 17:43:24 +0530

    Add dictionary and emoji converter scripts

---
+++ dictionary.py
@@ -0,0 +1,15 @@
+phone = str(input("Phone Number:"))
+number_mapping = {
+    "1": "One",
+    "2": "Two",
+    "3": "Three",
+    "4": "Four",
+    "5": "Five",
+    "6": "Six",
+    "7": "Seven",
+    "8": "Eight",
+    "9": "Nine",
+    "0": "Zero",
+    }
+for number in phone:
+    print(number_mapping.get(number, "!"))
